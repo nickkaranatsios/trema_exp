42ebcea398b96dd4bb40ba700089b84f419e9d7c
Nick Karanatsios <nickkaranatsios@gmail.com>
2013-01-23 21:50:51 +0900

    add more tests

--- ruby/test.rb
+++ ruby/test.rb
@@ -18,10 +18,19 @@ module Trema
         MatchIpProto.new( 17 ),
         MatchIpv4SrcAddr.new( "10.10.10.1" ),
         MatchIpv4DstAddr.new( "127.0.0.1" ),
-        MatchTcpSrcPort.new( 5555 ),
-        MatchTcpDstPort.new( 5556 ),
+        MatchTcpSrcPort.new( 3331 ),
+        MatchTcpDstPort.new( 3332 ),
         MatchUdpSrcPort.new( 3333 ),
         MatchUdpDstPort.new( 3334 ),
+        MatchSctpSrcPort.new( 3336 ),
+        MatchSctpDstPort.new( 3337 ),
+        MatchIcmpv4Type.new( 0 ), # echo reply
+        MatchIcmpv4Code.new( 2 ),
+        MatchArpOp.new( 1 ), # REQUEST
+        MatchArpSpa.new( "1.1.1.1" ),
+        MatchArpTpa.new( "1.1.1.2" ),
+        MatchArpSha.new( "11:22:33:44:55:67" ),
+        MatchArpTha.new( "11:22:33:44:55:68" ),
       ]
       test_match_set ms
     end
