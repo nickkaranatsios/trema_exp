1be21b56b1c254f459690bbce934ae8c62128567
Nick Karanatsios <nickkaranatsios@gmail.com>
2013-01-31 08:54:24 +0900

    add requires in ruby file

---
+++ ruby/trema/messages.rb
@@ -0,0 +1,5 @@
+require "trema/messages/hello"
+require "trema/messages/echo-request"
+require "trema/messages/features-request"
+require "trema/messages/get-config-request"
+require "trema/messages/set-config"
