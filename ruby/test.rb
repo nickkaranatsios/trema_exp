9997c8243cd84683fb6b6547dc9365873040aa70
Nick Karanatsios <nickkaranatsios@gmail.com>
2013-01-31 18:36:35 +0900

    add MatchSet module to match classes

--- ruby/test.rb
+++ ruby/test.rb
@@ -5,46 +5,46 @@ module Trema
   class Test < Controller
     def match_set
       ms = [ 
-        MatchInPort.new( 2 ),
-        MatchInPhyPort.new( 2 ), 
-        MatchMetadata.new( 2**34 ),
-        MatchEthDst.new( "11:22:33:44:55:66" ),
-        MatchEthSrc.new( "11:22:33:44:55:66" ),
-        MatchEtherType.new( 0x7777 ),
-        MatchVlanVid.new( 1024 ),
-        MatchVlanPriority.new( 7 ),
-        MatchIpDscp.new( 63 ),
-        MatchIpEcn.new( 43 ),
-        MatchIpProto.new( 17 ),
-        MatchIpv4SrcAddr.new( "10.10.10.1" ),
-        MatchIpv4DstAddr.new( "127.0.0.1" ),
-        MatchTcpSrcPort.new( 3331 ),
-        MatchTcpDstPort.new( 3332 ),
-        MatchUdpSrcPort.new( 3333 ),
-        MatchUdpDstPort.new( 3334 ),
-        MatchSctpSrcPort.new( 3336 ),
-        MatchSctpDstPort.new( 3337 ),
-        MatchIcmpv4Type.new( 0 ), # echo reply
-        MatchIcmpv4Code.new( 2 ),
-        MatchArpOp.new( 1 ), # REQUEST
-        MatchArpSpa.new( "1.1.1.1" ),
-        MatchArpTpa.new( "1.1.1.2" ),
-        MatchArpSha.new( "11:22:33:44:55:67" ),
-        MatchArpTha.new( "11:22:33:44:55:68" ),
-        MatchIpv6SrcAddr.new( "ffe1::1" ),
-        MatchIpv6DstAddr.new( "ffe2::2" ),
-        MatchIpv6FlowLabel.new( 10 ),
-        MatchIcmpv6Type.new( 1 ), # destination unreachable
-        MatchIcmpv6Code.new( 3 ), # address unreachable if as above type.
-        MatchIpv6NdTarget.new( "ffe3::3" ),
-        MatchIpv6NdSll.new( "11:22:33:44:55:69" ),
-        MatchIpv6NdTll.new( "11:22:33:44:55:70" ),
-        MatchMplsLabel.new( 0 ), # IPv4 Explicit NULL label
-        MatchMplsTc.new( 1 ), # TC refers to traffic class
-        MatchMplsBos.new( 1 ), # Bottom of stack a bit on/off.
-        MatchPbbIsid.new( 10 ), # Provider Backbone bridge i-sid ( service identifier ) 3 bytes
-        MatchTunnelId.new( rand( 2**64 ) ), # Tunnel id applies to GRE packets with RFC2890 key extension
-        MatchIpv6Exthdr.new( rand( 2**16 ) ),
+        MatchSet::MatchInPort.new( 2 ),
+        MatchSet::MatchInPhyPort.new( 2 ), 
+        MatchSet::MatchMetadata.new( 2**34 ),
+        MatchSet::MatchEthDst.new( "11:22:33:44:55:66" ),
+        MatchSet::MatchEthSrc.new( "11:22:33:44:55:66" ),
+        MatchSet::MatchEtherType.new( 0x7777 ),
+        MatchSet::MatchVlanVid.new( 1024 ),
+        MatchSet::MatchVlanPriority.new( 7 ),
+        MatchSet::MatchIpDscp.new( 63 ),
+        MatchSet::MatchIpEcn.new( 43 ),
+        MatchSet::MatchIpProto.new( 17 ),
+        MatchSet::MatchIpv4SrcAddr.new( "10.10.10.1" ),
+        MatchSet::MatchIpv4DstAddr.new( "127.0.0.1" ),
+        MatchSet::MatchTcpSrcPort.new( 3331 ),
+        MatchSet::MatchTcpDstPort.new( 3332 ),
+        MatchSet::MatchUdpSrcPort.new( 3333 ),
+        MatchSet::MatchUdpDstPort.new( 3334 ),
+        MatchSet::MatchSctpSrcPort.new( 3336 ),
+        MatchSet::MatchSctpDstPort.new( 3337 ),
+        MatchSet::MatchIcmpv4Type.new( 0 ), # echo reply
+        MatchSet::MatchIcmpv4Code.new( 2 ),
+        MatchSet::MatchArpOp.new( 1 ), # REQUEST
+        MatchSet::MatchArpSpa.new( "1.1.1.1" ),
+        MatchSet::MatchArpTpa.new( "1.1.1.2" ),
+        MatchSet::MatchArpSha.new( "11:22:33:44:55:67" ),
+        MatchSet::MatchArpTha.new( "11:22:33:44:55:68" ),
+        MatchSet::MatchIpv6SrcAddr.new( "ffe1::1" ),
+        MatchSet::MatchIpv6DstAddr.new( "ffe2::2" ),
+        MatchSet::MatchIpv6FlowLabel.new( 10 ),
+        MatchSet::MatchIcmpv6Type.new( 1 ), # destination unreachable
+        MatchSet::MatchIcmpv6Code.new( 3 ), # address unreachable if as above type.
+        MatchSet::MatchIpv6NdTarget.new( "ffe3::3" ),
+        MatchSet::MatchIpv6NdSll.new( "11:22:33:44:55:69" ),
+        MatchSet::MatchIpv6NdTll.new( "11:22:33:44:55:70" ),
+        MatchSet::MatchMplsLabel.new( 0 ), # IPv4 Explicit NULL label
+        MatchSet::MatchMplsTc.new( 1 ), # TC refers to traffic class
+        MatchSet::MatchMplsBos.new( 1 ), # Bottom of stack a bit on/off.
+        MatchSet::MatchPbbIsid.new( 10 ), # Provider Backbone bridge i-sid ( service identifier ) 3 bytes
+        MatchSet::MatchTunnelId.new( rand( 2**64 ) ), # Tunnel id applies to GRE packets with RFC2890 key extension
+        MatchSet::MatchIpv6Exthdr.new( rand( 2**16 ) ),
       ]
       test_match_set ms
     end
