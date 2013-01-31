e5d1ac38542d8a46d8bfe725e243029d51802525
Nick Karanatsios <nickkaranatsios@gmail.com>
2013-01-31 19:33:09 +0900

    new message

---
+++ ruby/trema/messages/flow-mod.rb
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2008-2013 NEC Corporation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+
+require "trema/message-accessor"
+
+
+module Trema
+  module Messages
+    class FlowMod < MessageAccessor 
+      unsigned_int64 :cookie
+      unsigned_int64 :cookie_mask
+      unsigned_int8 :command
+      unsigned_int16 :idle_timeout
+      unsigned_int16 :hard_timeout
+      unsigned_int16 :priority
+      unsigned_int32 :buffer_id
+      unsigned_int16 :flags
+      array :match
+    end
+  end
+end
+
+
+### Local variables:
+### mode: Ruby
+### coding: utf-8-unix
+### indent-tabs-mode: nil
+### End:
