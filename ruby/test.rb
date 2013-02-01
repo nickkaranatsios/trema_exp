bd0a46a1317a9aeffb7abcea6d9ca48efbb42c1b
Nick Karanatsios <nickkaranatsios@gmail.com>
2013-02-01 17:35:49 +0900

    rearrange actions

--- ruby/test.rb
+++ ruby/test.rb
@@ -3,53 +3,53 @@ $LOAD_PATH.unshift File.expand_path( File.join( File.dirname( __FILE__ ), "." ) 
 require "trema"
 module Trema
   class Test < Controller
-    def match_set
+    def flexible_actions
       ms = [ 
-        MatchSet::MatchInPort.new( 2 ),
-        MatchSet::MatchInPhyPort.new( 2 ), 
-        MatchSet::MatchMetadata.new( 2**34 ),
-        MatchSet::MatchEthDst.new( "11:22:33:44:55:66" ),
-        MatchSet::MatchEthSrc.new( "11:22:33:44:55:66" ),
-        MatchSet::MatchEtherType.new( 0x7777 ),
-        MatchSet::MatchVlanVid.new( 1024 ),
-        MatchSet::MatchVlanPriority.new( 7 ),
-        MatchSet::MatchIpDscp.new( 63 ),
-        MatchSet::MatchIpEcn.new( 43 ),
-        MatchSet::MatchIpProto.new( 17 ),
-        MatchSet::MatchIpv4SrcAddr.new( "10.10.10.1" ),
-        MatchSet::MatchIpv4DstAddr.new( "127.0.0.1" ),
-        MatchSet::MatchTcpSrcPort.new( 3331 ),
-        MatchSet::MatchTcpDstPort.new( 3332 ),
-        MatchSet::MatchUdpSrcPort.new( 3333 ),
-        MatchSet::MatchUdpDstPort.new( 3334 ),
-        MatchSet::MatchSctpSrcPort.new( 3336 ),
-        MatchSet::MatchSctpDstPort.new( 3337 ),
-        MatchSet::MatchIcmpv4Type.new( 0 ), # echo reply
-        MatchSet::MatchIcmpv4Code.new( 2 ),
-        MatchSet::MatchArpOp.new( 1 ), # REQUEST
-        MatchSet::MatchArpSpa.new( "1.1.1.1" ),
-        MatchSet::MatchArpTpa.new( "1.1.1.2" ),
-        MatchSet::MatchArpSha.new( "11:22:33:44:55:67" ),
-        MatchSet::MatchArpTha.new( "11:22:33:44:55:68" ),
-        MatchSet::MatchIpv6SrcAddr.new( "ffe1::1" ),
-        MatchSet::MatchIpv6DstAddr.new( "ffe2::2" ),
-        MatchSet::MatchIpv6FlowLabel.new( 10 ),
-        MatchSet::MatchIcmpv6Type.new( 1 ), # destination unreachable
-        MatchSet::MatchIcmpv6Code.new( 3 ), # address unreachable if as above type.
-        MatchSet::MatchIpv6NdTarget.new( "ffe3::3" ),
-        MatchSet::MatchIpv6NdSll.new( "11:22:33:44:55:69" ),
-        MatchSet::MatchIpv6NdTll.new( "11:22:33:44:55:70" ),
-        MatchSet::MatchMplsLabel.new( 0 ), # IPv4 Explicit NULL label
-        MatchSet::MatchMplsTc.new( 1 ), # TC refers to traffic class
-        MatchSet::MatchMplsBos.new( 1 ), # Bottom of stack a bit on/off.
-        MatchSet::MatchPbbIsid.new( 10 ), # Provider Backbone bridge i-sid ( service identifier ) 3 bytes
-        MatchSet::MatchTunnelId.new( rand( 2**64 ) ), # Tunnel id applies to GRE packets with RFC2890 key extension
-        MatchSet::MatchIpv6Exthdr.new( rand( 2**16 ) ),
+        Actions::InPort.new( 2 ),
+        Actions::InPhyPort.new( 2 ), 
+        Actions::Metadata.new( 2**34 ),
+        Actions::EthDst.new( "11:22:33:44:55:66" ),
+        Actions::EthSrc.new( "11:22:33:44:55:66" ),
+        Actions::EtherType.new( 0x7777 ),
+        Actions::VlanVid.new( 1024 ),
+        Actions::VlanPriority.new( 7 ),
+        Actions::IpDscp.new( 63 ),
+        Actions::IpEcn.new( 43 ),
+        Actions::IpProto.new( 17 ),
+        Actions::Ipv4SrcAddr.new( "10.10.10.1" ),
+        Actions::Ipv4DstAddr.new( "127.0.0.1" ),
+        Actions::TcpSrcPort.new( 3331 ),
+        Actions::TcpDstPort.new( 3332 ),
+        Actions::UdpSrcPort.new( 3333 ),
+        Actions::UdpDstPort.new( 3334 ),
+        Actions::SctpSrcPort.new( 3336 ),
+        Actions::SctpDstPort.new( 3337 ),
+        Actions::Icmpv4Type.new( 0 ), # echo reply
+        Actions::Icmpv4Code.new( 2 ),
+        Actions::ArpOp.new( 1 ), # REQUEST
+        Actions::ArpSpa.new( "1.1.1.1" ),
+        Actions::ArpTpa.new( "1.1.1.2" ),
+        Actions::ArpSha.new( "11:22:33:44:55:67" ),
+        Actions::ArpTha.new( "11:22:33:44:55:68" ),
+        Actions::Ipv6SrcAddr.new( "ffe1::1" ),
+        Actions::Ipv6DstAddr.new( "ffe2::2" ),
+        Actions::Ipv6FlowLabel.new( 10 ),
+        Actions::Icmpv6Type.new( 1 ), # destination unreachable
+        Actions::Icmpv6Code.new( 3 ), # address unreachable if as above type.
+        Actions::Ipv6NdTarget.new( "ffe3::3" ),
+        Actions::Ipv6NdSll.new( "11:22:33:44:55:69" ),
+        Actions::Ipv6NdTll.new( "11:22:33:44:55:70" ),
+        Actions::MplsLabel.new( 0 ), # IPv4 Explicit NULL label
+        Actions::MplsTc.new( 1 ), # TC refers to traffic class
+        Actions::MplsBos.new( 1 ), # Bottom of stack a bit on/off.
+        Actions::PbbIsid.new( 10 ), # Provider Backbone bridge i-sid ( service identifier ) 3 bytes
+        Actions::TunnelId.new( rand( 2**64 ) ), # Tunnel id applies to GRE packets with RFC2890 key extension
+        Actions::Ipv6Exthdr.new( rand( 2**16 ) ),
       ]
-      test_match_set ms
+      test_action_set ms
     end
-    def action_list
-      test_action_list build_action_list
+    def basic_actions
+      test_action_list build_basic_actions
     end
     def message_list
       ml = [
@@ -75,31 +75,32 @@ module Trema
     end
 
 
-    def build_action_list
+    def build_basic_actions
       [
-        ActionList::SendOutPort.new( :port_number => 2, :max_len => 2**7 ),
-        ActionList::GroupAction.new( :group_id => 1 ),
-        ActionList::CopyTtlIn.new,
-        ActionList::CopyTtlOut.new,
-        ActionList::SetMplsTtl.new( 10 ),
-        ActionList::DecMplsTtl.new,
-        ActionList::PushVlan.new( 0x88a8 ),
-        ActionList::PopVlan.new,
-        ActionList::PushMpls.new( 0x8847 ),
-        ActionList::PopMpls.new( 0x8848 ),
-        ActionList::SetQueue.new( rand( 2**32 ) ),
-        ActionList::SetIpTtl.new( rand( 2** 8 ) ),
-        ActionList::PushPbb.new( rand( 2**16 ) ),
-        ActionList::PopPbb.new,
-        ActionList::Experimenter.new( :experimenter => rand( 2 ** 32 ), :body => "abcdef".unpack( "C*" ) ),
+        Actions::SendOutPort.new( :port_number => 2, :max_len => 2**7 ),
+        Actions::GroupAction.new( :group_id => 1 ),
+        Actions::CopyTtlIn.new,
+        Actions::CopyTtlOut.new,
+        Actions::SetMplsTtl.new( 10 ),
+        Actions::DecMplsTtl.new,
+        Actions::PushVlan.new( 0x88a8 ),
+        Actions::PopVlan.new,
+        Actions::PushMpls.new( 0x8847 ),
+        Actions::PopMpls.new( 0x8848 ),
+        Actions::SetQueue.new( rand( 2**32 ) ),
+        Actions::SetField.new( :action_set => [ Actions::VlanVid.new( rand( 4096 ) ) ] ),
+        Actions::SetIpTtl.new( rand( 2** 8 ) ),
+        Actions::PushPbb.new( rand( 2**16 ) ),
+        Actions::PopPbb.new,
+        Actions::Experimenter.new( :experimenter => rand( 2 ** 32 ), :body => "abcdef".unpack( "C*" ) ),
       ]
     end
   end
 end
 
 t = Trema::Test.new
-t.action_list
-t.match_set
-t.message_list
-t.instruction_list
+t.basic_actions
+t.flexible_actions
+#t.message_list
+#t.instruction_list
 
